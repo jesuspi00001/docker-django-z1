e2c58b66fc41d633abc90cae3482a51c6162bcd4
jesuspereira <jesuspi00001@email.com>
2023-10-04 16:11:41 +0200

    Version 2: un usuario es capaz de logarse utilizando email y pass.

--- users/models.py
+++ users/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class User(models.Model):
-    username = models.CharField(max_length=255, unique=True, error_messages={'unique': 'Esta dirección de correo electrónico ya está en uso.'})
+class User(AbstractUser):
+    username = models.CharField(max_length=30, unique=True, error_messages={'unique': 'Esta dirección de correo electrónico ya está en uso.'})
     email = models.EmailField()
-    password = models.CharField(max_length=25)
+    password = models.CharField(max_length=255)
 
     def __str__(self):
         return self.username
--- users/schema.py
+++ users/schema.py
@@ -1,5 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
+import graphql_jwt
+from graphene_django.types import DjangoObjectType
 from users.models import User
 
 
@@ -26,14 +28,17 @@ class CreateUser(graphene.Mutation):
 
 class DeleteUser(graphene.Mutation):
     class Arguments:
-        id = graphene.ID(required=True)
+        username = graphene.String(required=True)
 
     message = graphene.String()
 
     def mutate(self, info, username):
-        user = User.objects.get(pk=username)
-        user.delete()
-        return DeleteUser(message="Usuario eliminado")
+        try:
+            user = User.objects.get(username=username)
+            user.delete()
+            return DeleteUser(message=f"Usuario {username} eliminado correctamente")
+        except User.DoesNotExist:
+            return DeleteUser(message=f"Usuario {username} no encontrado")
 
 
 class UpdateUser(graphene.Mutation):
@@ -52,6 +57,17 @@ class UpdateUser(graphene.Mutation):
         user.password = password
         user.save()
         return UpdateUser(user=user)
+    
+class TokenAuthWithEmail(graphql_jwt.JSONWebTokenMutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    @classmethod
+    def resolve(cls, root, info, **kwargs):
+        return cls(user=info.context.user)
 
 
 class Query(graphene.ObjectType):
@@ -70,5 +86,6 @@ class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
     delete_user = DeleteUser.Field()
     update_user = UpdateUser.Field()
+    token_auth_with_email = TokenAuthWithEmail.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
